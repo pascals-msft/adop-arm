704707da09312902820740bdcfe11c52ae6de9a6
Pascal Sauliere <pascals@microsoft.com>
2016-09-26 00:00:42 +0200

    Create startup.sh

---
+++ startup.sh
@@ -0,0 +1,62 @@
+#!/bin/bash 
+
+OVERRIDES=
+
+
+echo ' 
+      ###    ########   #######  ########  
+     ## ##   ##     ## ##     ## ##     ## 
+    ##   ##  ##     ## ##     ## ##     ## 
+   ##     ## ##     ## ##     ## ########  
+   ######### ##     ## ##     ## ##        
+   ##     ## ##     ## ##     ## ##        
+   ##     ## ########   #######  ##        
+'
+
+usage(){
+  cat <<END_USAGE
+
+Usage: ./startup.sh
+   -u <INITIAL_ADMIN_USER>
+   -p <INITIAL_ADMIN_PASSWORD>
+
+END_USAGE
+}
+
+
+while getopts "u:p:" opt; do
+  case $opt in
+	u)
+      export INITIAL_ADMIN_USER=${OPTARG}
+	      ;;
+    p)
+      export INITIAL_ADMIN_PASSWORD_PLAIN=${OPTARG}
+	      ;;
+    *)
+      echo "Invalid parameter(s) or option(s)."
+      usage
+      exit 1
+      ;;
+  esac
+done
+
+
+if [ -z $INITIAL_ADMIN_USER ] | [ -z $INITIAL_ADMIN_PASSWORD_PLAIN ]; then
+  usage
+  exit 1
+fi
+
+# install latest version of compose
+curl -L https://github.com/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+# download and unzip ADOP
+apt-get update
+apt-get install unzip
+cd /tmp
+git clone https://github.com/Accenture/adop-docker-compose.git
+cd adop-docker-compose
+
+# install ADOP
+
+./adop compose init
